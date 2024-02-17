96e7b443ea0720b71cc7587aa803994c2e23f5c8
Avah Whitehead <avah.r.whitehead@gmail.com>
2024-02-17 17:16:13 +0000

    Changed to using newtonsoft json

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,31 +1,45 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace TodoApi;
 
 public class Program {
 	public static void Main(string[] args) {
 		var builder = WebApplication.CreateBuilder(args);
 
-		// Add services to the container.
+		//Initialise controllers
+		builder.Services.AddControllers()
+			//Use Newtonsoft for JSOn conversion
+			.AddNewtonsoftJson(options => {
+				//Convert enums to their string values instead of numerical
+				options.SerializerSettings.Converters.Add(new StringEnumConverter());
+				//Don't write null values
+				options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+			});
 
-		builder.Services.AddControllers();
-		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		//Add Swagger description to the endpoints
+		//Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 		builder.Services.AddEndpointsApiExplorer();
 		builder.Services.AddSwaggerGen();
 
 		var app = builder.Build();
 
 		// Configure the HTTP request pipeline.
-		if (app.Environment.IsDevelopment()) {
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
 
+		//Always use swagger
+		app.UseSwagger();
+		app.UseSwaggerUI();
+
+		//Redirect to HTTPs
 		app.UseHttpsRedirection();
 
+		//Enable Authorization
 		app.UseAuthorization();
 
-
+		//Map controllers
 		app.MapControllers();
 
+		//Start listening
 		app.Run();
 	}
 }
